1fae08ce73a725473feef44c815b29f3de29df3e
Kishore <kishmeister26@users.noreply.github.com>
2019-12-11 15:59:06 +0530

    redux reducers

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+
+//Redux 
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+
+const App = () => (
+  <Provider store={store}>  
+  <Router> 
+    <Fragment>
+      <Navbar />
+      <Route path="/" exact component={ Landing } />
+      <section className="container">
+      <Alert />
+        <Switch>
+          <Route path="/register" exact component={ Register } />
+          <Route path="/login" exact component={ Login } />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+  </Provider>
+);
+
+
+
+export default App;
